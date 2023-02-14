13070b3edbe64093d84bc551327b62e6e603e305
Azizbek <you@example.com>
2023-02-14 20:42:20 +0500

    v1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'/my-first'
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/Vue-Chat/' : '/'
+});
